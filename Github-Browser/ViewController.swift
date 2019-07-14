d1092392b05cba3e1570a4d5abf153f775553b24
Szymon Hanzel <szyhan@gmail.com>
2019-07-14 20:31:15 +0200

    Initial Commit

---
+++ Github-Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github-Browser
+//
+//  Created by Hanzel, Szymon on 14/07/2019.
+//  Copyright © 2019 Hanzel, Szymon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Github-BrowserTests/Github_BrowserTests.swift
@@ -0,0 +1,34 @@
+//
+//  Github_BrowserTests.swift
+//  Github-BrowserTests
+//
+//  Created by Hanzel, Szymon on 14/07/2019.
+//  Copyright © 2019 Hanzel, Szymon. All rights reserved.
+//
+
+import XCTest
+@testable import Github_Browser
+
+class Github_BrowserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
