71a791fef4cb9afd313359e2976ce172c21639df
Szymon Hanzel <szyhan@gmail.com>
2019-07-18 07:16:28 +0200

    upgraded main search field to modern design, removed description label

--- Github-Browser/Extensions/UIColor_+_init.swift
+++ Github-Browser/Extensions/UIColor_+_init.swift
@@ -13,6 +13,8 @@ extension UIColor {
     static var checkboxBlue: UIColor {return UIColor.init(rgb: 0x74b9ff)}
     static var grayOne: UIColor {return UIColor.init(rgb: 0xb2bec3)}
     static var iphoneBlue: UIColor{return UIColor.init(rgb: 0x5fc9f8)}
+    static var lighteriPhoneBlue: UIColor {return UIColor.init(rgb: 0x99DDFA)}
+    
     
     convenience init(red: Int, green: Int, blue: Int) {
         self.init(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: 1.0)
--- Github-Browser/RepositoriesBrowser/View/SHTopView.swift
+++ Github-Browser/RepositoriesBrowser/View/SHTopView.swift
@@ -14,34 +14,28 @@ class SHTopView: UIView {
     
     
     let titleLabel = SHLabel(text: "Repository Browser", color: .white, size: 26, textAlign: .center, font: .robotoBold)
-    let subtitleLabel = SHLabel(text: "Fill the search field below and use the filters to search through Git Hub repositories", color: .white, size: 12, textAlign: .justified)
-    
-    let searchIcon:UIImageView = {
-       let si = UIImageView(frame: CGRect(x: 0, y: 0, width: 32, height: 32))
-        si.translatesAutoresizingMaskIntoConstraints = false
-        si.image        = UIImage(named: "mini-search")
-        si.contentMode  = .scaleAspectFit
-        return si
-    }()
+
     
     
-    let searchTextField: SearchTextField = {
-        let textField = SearchTextField()
+    let searchTextField: SHMainSearchTextField = {
+        let textField = SHMainSearchTextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.theme.bgColor = .white
+        //textField.theme.bgColor = .black
+        textField.backgroundColor = .lighteriPhoneBlue
+        textField.layer.cornerRadius = 10
         textField.filterStrings(["red","yellow"])
         //creating header for the Last Searched hints
         textField.customHeaderwithText("Last Searched")
         textField.textColor     = .white
         textField.font          = UIFont.init(font: .robotoRegular, size: 16)
         let placeHolderFont     = UIFont.init(font: .robotoLightItalic, size: 13)
-        textField.customAttributedPlaceholderWithFont(placeHolderFont, text: "Fill this field with search text", andFontColor: .white)
+        textField.customAttributedPlaceholderWithFont(placeHolderFont, text: "Search", andFontColor: .white)
         //adding white bottom border to the field
-        textField.addBottomBorder(color: UIColor.white, margins: 0, borderLineSize: 1)
+        textField.disableAutoTypes()
         textField.returnKeyType = UIReturnKeyType.go
-        //creating maginfier icon for textField leftView
         //startVisible=true allows to show last searched queries on field focus
         textField.startVisible  = true
+        textField.setMagnifierIcon()
         return textField
     }()
     
@@ -55,8 +49,6 @@ class SHTopView: UIView {
         backgroundColor = .iphoneBlue
         
         self.addSubview(titleLabel)
-        self.addSubview(subtitleLabel)
-        self.addSubview(searchIcon)
         self.addSubview(searchTextField)
         fillUI()
         
@@ -66,29 +58,15 @@ class SHTopView: UIView {
         
         titleLabel.snp.makeConstraints { (make) in
             make.top.equalTo(safeAreaLayoutGuide)
-            make.centerX.equalTo(self.snp_centerX)
-        }
-        subtitleLabel.snp.makeConstraints { (make) in
-            make.centerX.equalTo(self)
-            make.width.equalTo(self.snp_width).multipliedBy(0.80)
+            make.left.equalTo(self).offset(20)
         }
         
         searchTextField.snp.makeConstraints{(make) in
-            make.leading.trailing.equalTo(subtitleLabel)
+            make.width.equalTo(self).multipliedBy(0.8)
             make.bottom.equalTo(self).offset(-5)
             make.centerX.equalTo(self)
+            make.top.equalTo(titleLabel.snp_bottom).offset(20)
             make.height.equalTo(25)
-        }
-        
-        searchIcon.snp.makeConstraints { (make) in
-            make.right.equalTo(searchTextField.snp_left)
-            make.bottom.equalTo(self).offset(-5)
-            make.height.equalTo(15)
-        }
-        
-        subtitleLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(titleLabel.snp_bottom)
-            make.bottom.equalTo(searchTextField.snp_top)
         }
         
         self.sizeToFitCustom()
