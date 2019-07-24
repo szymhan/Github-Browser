f3957e8770e57420666ed0b26cae4838565d932c
Szymon Hanzel <szyhan@gmail.com>
2019-07-24 10:44:54 +0200

    change alertController tob bar content to light

---
+++ Github-Browser/Extensions/AlertController_+_lightContent.swift
@@ -0,0 +1,16 @@
+//
+//  AlertController_+_lightContent.swift
+//  Github-Browser
+//
+//  Created by Hanzel, Szymon on 24/07/2019.
+//  Copyright © 2019 Hanzel, Szymon. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIAlertController {
+    override open var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
